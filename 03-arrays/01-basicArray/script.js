393750a5c6c29269142c097025c45b3e05031b38
Kalki <kalkithewarrior2706@proton.me>
2023-12-29 04:48:15 +0530

    Add basic array

---
+++ 03-arrays/01-basicArray/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const avgTempJan = 31.9,
+  avgTempFeb = 35.3,
+  avgTempMar = 42.4,
+  avgTempApr = 52,
+  avgTempMay = 60.8;
+
+const avgTemp = [];
+avgTemp.push(avgTempJan, avgTempFeb, avgTempMar, avgTempApr, avgTempMay);
+console.log(avgTemp);
